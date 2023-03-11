41e59d8ca1d3b8c01da4890484ecf6f92c6ae267
Konstantin Runov <runebone1@gmail.com>
2023-03-11 23:01:33 +0300

    init edges

--- lab1/lab1/edges.cpp
+++ lab1/lab1/edges.cpp
@@ -1,1 +1,12 @@
 #include "edges.h"
+
+static void init_edge_array(edge_array_t &edge_array)
+{
+    edge_array.size = 0;
+    edge_array.array = NULL;
+}
+
+void init_edges(edges_t &edges)
+{
+    init_edge_array(edges);
+}
