d9fea28ed783cc65e6ceb5f319387f4f7eba9578
Konstantin Runov <runebone1@gmail.com>
2023-03-12 11:06:10 +0300

    figure: return OK error_codes everywhere for now

--- lab1/lab1/figure.cpp
+++ lab1/lab1/figure.cpp
@@ -14,28 +14,46 @@ figure_t &init_figure()
 
 err_t load_figure(figure_t &figure, const filename_t &filename)
 {
+    err_t error_code = OK;
+
+    return error_code;
 }
 
 err_t save_figure(const figure_t &figure, const filename_t &filename)
 {
+    err_t error_code = OK;
+
+    return error_code;
 }
 
 
 err_t draw_figure(const figure_t &figure, const canvas_t &canvas)
 {
+    err_t error_code = OK;
+
+    return error_code;
 }
 
 
 err_t translate_figure(figure_t &figure, const change_t &change)
 {
+    err_t error_code = OK;
+
+    return error_code;
 }
 
 err_t rotate_figure(figure_t &figure, const change_t &change)
 {
+    err_t error_code = OK;
+
+    return error_code;
 }
 
 err_t scale_figure(figure_t &figure, const change_t &change)
 {
+    err_t error_code = OK;
+
+    return error_code;
 }
 
 
