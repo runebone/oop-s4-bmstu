4b3adef67660aecc7403dea7018298460950b402
Konstantin Runov <runebone1@gmail.com>
2023-03-12 18:47:00 +0300

    draw: draw_wireframe; minor improvements

--- lab1/lab1/qt/draw.cpp
+++ lab1/lab1/qt/draw.cpp
@@ -28,14 +28,21 @@ static double get_canvas_height(const canvas_t &canvas)
     return height;
 }
 
-err_t canvas_exists(const canvas_t &canvas)
+static err_t scene_exists(const QGraphicsScene *scene)
 {
     err_t error_code = OK;
 
-    if (canvas.scene == nullptr)
+    if (scene == nullptr)
     {
         error_code = ERR_NULL_SCENE;
     }
+
+    return error_code;
+}
+
+err_t canvas_exists(const canvas_t &canvas)
+{
+    err_t error_code = scene_exists(canvas.scene);
 
     return error_code;
 }
@@ -48,4 +55,63 @@ static void clear_scene(QGraphicsScene *scene)
 void clear_canvas(const canvas_t &canvas)
 {
     clear_scene(canvas.scene);
+}
+
+static void qt_draw_line(QGraphicsScene *scene, const point_t &point_1, const point_t &point_2)
+{
+    QPen pen = QPen(Qt::black, 2);
+
+    scene->addLine(point_1.x, point_1.y, point_2.x, point_2.y, pen);
+}
+
+static void center_point(point_t &point, const canvas_t &canvas)
+{
+    // XXX: this requires canvas.scene to have fixed width and height to work properly
+    double width = get_canvas_width(canvas);
+    double height = get_canvas_height(canvas);
+
+    point.x += width / 2;
+    point.y += height / 2;
+}
+
+static void draw_line(const canvas_t &canvas, const point_t &point_1, const point_t &point_2)
+{
+    point_t centered_point_1 = point_1;
+    point_t centered_point_2 = point_2;
+
+    center_point(centered_point_1, canvas);
+    center_point(centered_point_2, canvas);
+
+    qt_draw_line(canvas.scene, centered_point_1, centered_point_2);
+}
+
+err_t draw_wireframe(const canvas_t &canvas, const points_t &points, const edges_t &edges)
+{
+    err_t error_code = OK;
+
+    point_t point_1;
+    point_t point_2;
+    edge_t edge;
+
+    for (int i = 0; error_code == OK && i < edges.size; i++)
+    {
+        error_code = get_edge_by_index(edge, i, edges);
+
+        if (error_code == OK)
+        {
+            error_code = get_point_by_index(point_1, edge.point_index_1, points);
+        }
+
+        if (error_code == OK)
+        {
+            error_code = get_point_by_index(point_2, edge.point_index_2, points);
+        }
+
+        if (error_code == OK)
+        {
+            draw_line(canvas, point_1, point_2);
+        }
+    }
+
+    return error_code;
 }
--- lab1/lab1/qt/draw.h
+++ lab1/lab1/qt/draw.h
@@ -3,6 +3,8 @@
 
 #include <QGraphicsScene>
 #include "errors.h"
+#include "points.h"
+#include "edges.h"
 
 struct canvas
 {
@@ -12,5 +14,7 @@ using canvas_t = struct canvas;
 
 err_t canvas_exists(const canvas_t &canvas);
 void clear_canvas(const canvas_t &canvas);
+
+err_t draw_wireframe(const canvas_t &canvas, const points_t &points, const edges_t &edges);
 
 #endif // DRAW_H
