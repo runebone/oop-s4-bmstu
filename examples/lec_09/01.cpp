eead048d56f13da0a7d046b6a1c486b736301fa1
Konstantin Runov <runebone1@gmail.com>
2023-04-04 12:54:59 +0300

    01: add comments

--- examples/lec_09/01.cpp
+++ examples/lec_09/01.cpp
@@ -21,10 +21,24 @@ struct PrintHelper<T, true>
 template <typename T>
 void print(const T& t)
 {
+    /* https://en.cppreference.com/w/cpp/types/is_pointer */
+    /* std::is_pointer */
+    /* Checks whether T is a pointer to object or a pointer to function (but
+     * not a pointer to member/member function) or a cv-qualified version
+     * thereof. Provides the member constant value which is equal to true, if T
+     * is an object/function pointer type. Otherwise, value is equal to false.
+     * */ 
+    /* inline constexpr bool is_pointer_v = is_pointer<T>::value; */
     PrintHelper<T, is_pointer_v<T>>::print(t);
 }
 
 #elif defined(PRIM_2)
+
+/* https://en.cppreference.com/w/cpp/types/integral_constant */
+/* true_type 	std::integral_constant<bool, true> */
+/* false_type 	std::integral_constant<bool, false> */
+/* std::integral_constant wraps a static constant of specified type. It is the
+ * base class for the C++ type traits. */ 
 template <typename T>
 void printHelper(false_type, const T& t) { cout << t << endl; }
 
@@ -34,10 +48,16 @@ void printHelper(true_type, const T& t) { cout << *t << endl; }
 template <typename T>
 void print(const T& t)
 {
+    /* https://en.cppreference.com/w/cpp/types/is_pointer */
+    /* type 	std::integral_constant<bool, value> */
     printHelper(typename is_pointer<T>::type{}, t);
 }
 
 #elif defined(PRIM_3)
+
+/* https://en.cppreference.com/w/cpp/language/constexpr */
+/* The constexpr specifier declares that it is possible to evaluate the value
+ * of the function or variable at compile time. */ 
 template <typename T>
 void print(const T& t)
 {
@@ -68,9 +88,13 @@ void print(const auto& t)
     cout << t << endl;
 }
 
+/* https://en.cppreference.com/w/cpp/language/constraints */
+/* A concept is a named set of requirements. The definition of a concept must
+ * appear at namespace scope. */ 
 template <typename T>
 concept pointer = is_pointer_v<T>;
 
+// t is a reference to any type, which satisfies the "pointer" concept
 void print(const pointer auto& t)
 {
     cout << *t << endl;
