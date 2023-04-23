faffbb8957b22a0b75d1dbf970abacf0632060aa
Konstantin Runov <runebone1@gmail.com>
2023-04-23 18:00:03 +0300

    add: base_error.h

---
+++ lab2/base_error.h
@@ -0,0 +1,36 @@
+#ifndef __BASE_ERROR_H__
+#define __BASE_ERROR_H__
+
+#include <exception>
+#include <string>
+
+class BaseError : public std::exception
+{
+public:
+    explicit BaseError(
+        const std::string &errorname,
+        const std::string &filename,
+        const std::string &classname,
+        const std::string &funcname,
+        const int line,
+        const char *time,
+        const std::string &info)
+    {
+        msg = "In file: " + filename + "\n"
+              + "inside: " + classname + "\n"
+              + "in function: " + funcname + "\n"
+              + "at line: " + std::to_string(line) + "\n"
+              + "at: " + time + "\n"
+              + errorname + " occured: " + info;
+    }
+
+    [[nodiscard]] const char *what() const noexcept override
+    {
+        return msg.c_str();
+    }
+
+private:
+    std::string msg{};
+};
+
+#endif // __BASE_ERROR_H__
