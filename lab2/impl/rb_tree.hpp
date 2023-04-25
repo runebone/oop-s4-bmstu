874506c9dfbf05f1c3ace00401cd2824e54fc52e
Konstantin Runov <runebone1@gmail.com>
2023-04-25 13:58:35 +0300

    add: some iterator implementations

--- lab2/impl/rb_tree.hpp
+++ lab2/impl/rb_tree.hpp
@@ -2,6 +2,10 @@
 #define __RB_TREE_IMPL__
 
 #include "../rb_tree.h"
+#include "rb_tree_errors.h"
+
+template<typename T>
+using Node = typename RedBlackTree<T>::Node;
 
 template<typename T>
 using NodePtr = typename RedBlackTree<T>::NodePtr;
@@ -12,44 +16,136 @@ using WeakNodePtr = typename RedBlackTree<T>::WeakNodePtr;
 template<typename T>
 using Iterator = typename RedBlackTree<T>::Iterator;
 
-// XXX
 template<typename T>
-using difference_type = typename RedBlackTree<T>::Iterator::difference_type;
+using difference_type = typename RedBlackTree<T>::Iterator::difference_type; // XXX
+
+template<typename T>
+NodePtr<T> next(NodePtr<T> node)
+{
+    NodePtr<T> next_ptr = node->right;
+
+    if (next_ptr == nullptr)
+    {
+        if (node->parent == nullptr)
+        {
+            time_t timer = time(nullptr);
+            throw OrphanNodeError(__FILE__, "Node<T>", __func__, __LINE__, ctime(&timer));
+        }
+
+        NodePtr<T> parent = node->parent;
+
+        if (next_ptr == parent->left)
+        {
+            next_ptr = parent;
+        }
+        else if (next_ptr == parent->right)
+        {
+            next_ptr = parent->parent;
+        }
+    }
+    else
+    {
+        while (next_ptr->left != nullptr)
+        {
+            next_ptr = next_ptr->left;
+        }
+    }
+
+    return next_ptr;
+}
+
+template<typename T>
+NodePtr<T> prev(NodePtr<T> node)
+{
+    NodePtr<T> prev_ptr = node->left;
+
+    if (prev_ptr == nullptr)
+    {
+        if (node->parent == nullptr)
+        {
+            time_t timer = time(nullptr);
+            throw OrphanNodeError(__FILE__, "Node<T>", __func__, __LINE__, ctime(&timer));
+        }
+
+        NodePtr<T> parent = node->parent;
+
+        if (prev_ptr == parent->left)
+        {
+            prev_ptr = parent->parent;
+        }
+        else if (prev_ptr == parent->right)
+        {
+            prev_ptr = parent;
+        }
+    }
+    else
+    {
+        while (prev_ptr->right != nullptr)
+        {
+            prev_ptr = prev_ptr->right;
+        }
+    }
+
+    return prev_ptr;
+}
 
 #pragma region Iterator
+/* explicit Iterator(const Iterator& other); */
+template<typename T>
+RedBlackTree<T>::Iterator::Iterator(const Iterator& other)
+    : m_current(other.m_current), m_index(other.m_index)
+{
+}
+
+/* Iterator(Iterator&& other) noexcept; */
+template<typename T>
+RedBlackTree<T>::Iterator::Iterator(Iterator&& other) noexcept
+    : m_current(other.m_current), m_index(other.m_index)
+{
+}
+
 /* const T&  operator*() const; */
 template<typename T>
 const T& RedBlackTree<T>::Iterator::operator*() const
 {
-    // TODO
+    return m_current.lock()->data;
 }
 
 /* const T*  operator->() const; */
 template<typename T>
 const T* RedBlackTree<T>::Iterator::operator->() const
 {
-    // TODO
+    return &(m_current.lock()->data);
 }
 
 /* Iterator& operator++(); */
 template<typename T>
 Iterator<T>& RedBlackTree<T>::Iterator::operator++()
 {
-    // TODO
+    m_current = next(m_current);
+    ++m_index;
+
+    return *this;
 }
 
 /* Iterator  operator++(int); */
 template<typename T>
 Iterator<T> RedBlackTree<T>::Iterator::operator++(int)
 {
-    // TODO
+    Iterator tmp(*this);
+    ++(*this);
+
+    return tmp;
 }
 
 /* Iterator& operator--(); */
 template<typename T>
 Iterator<T>& RedBlackTree<T>::Iterator::operator--()
 {
-    // TODO
+    m_current = prev(m_current);
+    --m_index;
+
+    return *this;
 }
 
 
@@ -57,63 +153,82 @@ Iterator<T>& RedBlackTree<T>::Iterator::operator--()
 template<typename T>
 Iterator<T> RedBlackTree<T>::Iterator::operator--(int)
 {
-    // TODO
+    Iterator tmp(*this);
+    --(*this);
+
+    return tmp;
 }
 
 /* bool      operator!=(const Iterator& other) const; */
 template<typename T>
 bool RedBlackTree<T>::Iterator::operator!=(const Iterator& other) const
 {
-    // TODO
+    return m_index != other.m_index;
 }
 
 /* bool      operator==(const Iterator& other) const; */
 template<typename T>
 bool RedBlackTree<T>::Iterator::operator==(const Iterator& other) const
 {
-    // TODO
+    return m_index == other.m_index;
 }
 
 /* Iterator  operator+(difference_type n) const; */
 template<typename T>
-Iterator<T> RedBlackTree<T>::Iterator::operator+(difference_type n) const
+Iterator<T> RedBlackTree<T>::Iterator::operator+(const difference_type& n) const
 {
-    // XXX: can you define difference_type like that?
+    Iterator it(*this);
+
+    for (auto count = n; count > 0; ++it, --count);
+
+    return it;
 }
 
 /* Iterator  operator-(difference_type n) const; */
 template<typename T>
-Iterator<T> RedBlackTree<T>::Iterator::operator-(difference_type n) const
+Iterator<T> RedBlackTree<T>::Iterator::operator-(const difference_type& n) const
 {
-    // TODO
+    Iterator it(*this);
+
+    for (auto count = n; count > 0; --it, --count);
+
+    return it;
 }
 
 /* Iterator& operator+=(difference_type n); */
 template<typename T>
-Iterator<T>& RedBlackTree<T>::Iterator::operator+=(difference_type n)
+Iterator<T>& RedBlackTree<T>::Iterator::operator+=(const difference_type& n)
 {
-    // TODO
+    for (auto count = n; count > 0; ++(*this), --count);
+
+    return *this;
 }
 
 /* Iterator& operator-=(difference_type n); */
 template<typename T>
-Iterator<T>& RedBlackTree<T>::Iterator::operator-=(difference_type n)
+Iterator<T>& RedBlackTree<T>::Iterator::operator-=(const difference_type& n)
 {
-    // TODO
+    for (auto count = n; count > 0; --(*this), --count);
+
+    return *this;
 }
 
 /* const T&  operator[](difference_type n) const; */
 template<typename T>
-const T& RedBlackTree<T>::Iterator::operator[](difference_type n) const
+const T& RedBlackTree<T>::Iterator::operator[](const difference_type& n) const
 {
-    // TODO
+    Iterator it(*this);
+
+    it = it - it.m_index + n;
+
+    return *it; // XXX
 }
 
 /* explicit  operator bool() const; */
 template<typename T>
 RedBlackTree<T>::Iterator::operator bool() const
 {
-    // TODO
+    return !(m_current.expired());
 }
 #pragma endregion // Iterator
 
@@ -129,6 +244,13 @@ RedBlackTree<T>::RedBlackTree(const RedBlackTree& other)
 /* RedBlackTree(RedBlackTree&& other) noexcept; */
 template<typename T>
 RedBlackTree<T>::RedBlackTree(RedBlackTree&& other) noexcept
+{
+    // TODO
+}
+
+/* RedBlackTree(std::initializer_list<T> l); */
+template<typename T>
+RedBlackTree<T>::RedBlackTree(std::initializer_list<T> l)
 {
     // TODO
 }
@@ -198,13 +320,6 @@ Iterator<T> RedBlackTree<T>::end() const
     // TODO
 }
 
-/* Iterator find(const T& value) const; */
-template<typename T>
-Iterator<T> RedBlackTree<T>::find(const T& value) const
-{
-    // TODO
-}
-
 /* RedBlackTree& operator=(const RedBlackTree& other); */
 template<typename T>
 RedBlackTree<T>& RedBlackTree<T>::operator=(const RedBlackTree<T>& other)
@@ -229,42 +344,42 @@ bool RedBlackTree<T>::operator==(const RedBlackTree& other) const
 /* protected: */
 /* void rotate_right(NodePtr node); */
 template<typename T>
-void RedBlackTree<T>::rotate_right(NodePtr node)
+void rotate_right(NodePtr<T> node)
 {
     // TODO
 }
 
 /* void rotate_left(NodePtr node); */
 template<typename T>
-void RedBlackTree<T>::rotate_left(NodePtr node)
+void rotate_left(NodePtr<T> node)
 {
     // TODO
 }
 
 /* void fixup_insert(NodePtr node); */
 template<typename T>
-void RedBlackTree<T>::fixup_insert(NodePtr node)
+void fixup_insert(NodePtr<T> node)
 {
     // TODO
 }
 
 /* void fixup_remove(NodePtr node); */
 template<typename T>
-void RedBlackTree<T>::fixup_remove(NodePtr node)
+void fixup_remove(NodePtr<T> node)
 {
     // TODO
 }
 
 /* bool is_red(NodePtr node) const; */
 template<typename T>
-bool RedBlackTree<T>::is_red(NodePtr node) const
+bool is_red(NodePtr<T> node)
 {
     // TODO
 }
 
 /* void change_color(NodePtr node); */
 template<typename T>
-void RedBlackTree<T>::change_color(NodePtr node)
+void change_color(NodePtr<T> node)
 {
     // TODO
 }
@@ -278,7 +393,7 @@ NodePtr<T> RedBlackTree<T>::search(const T& key) const
 
 /* bool validate() const; */
 template<typename T>
-bool RedBlackTree<T>::validate() const
+bool validate()
 {
     // TODO
 }
