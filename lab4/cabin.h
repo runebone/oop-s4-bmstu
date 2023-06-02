e6df0a93723f235c5178637afb3f2fdaf503dce8
Konstantin Runov <runebone1@gmail.com>
2023-06-02 21:15:13 +0300

    aoaoa a lot of cabin stuff

--- lab4/cabin.h
+++ lab4/cabin.h
@@ -1,3 +1,197 @@
 #pragma once
 
-#define CABIN_MOVE_ONE_FLOOR 2000
+#define CABIN_MOVE_ONE_FLOOR 5000
+
+#include <string>
+#include <boost/asio.hpp>
+
+#include "doors.h"
+#include "writer.h"
+#include "timer.h"
+
+class Cabin
+{
+public:
+    enum State
+    {
+        Idling,
+        Waiting,
+        MovingUp,
+        MovingDown
+    };
+
+    Cabin(boost::asio::io_context &ioContext) : Cabin(ioContext, Writer()) {}
+    Cabin(boost::asio::io_context &ioContext, Writer &&writer) : Cabin(ioContext, writer) {}
+    Cabin(boost::asio::io_context &ioContext, Writer &writer)
+        : m_context(ioContext),
+            m_doors(ioContext, writer),
+            m_writer(writer),
+            m_moveTimer(ioContext)
+    {
+        m_doors.set_on_opened_callback([this](){
+                update(Waiting);
+            });
+
+        m_doors.set_on_closed_callback([this](){
+                update(Idling);
+            });
+    }
+
+    void open_doors()
+    {
+        switch (m_state)
+        {
+            case Idling:
+            case Waiting:
+                m_doors.open();
+                break;
+            case MovingUp:
+            case MovingDown:
+                write("Кабина не может открыть двери во время движения. Игнорируем запрос.");
+                break;
+        }
+    }
+
+    void close_doors()
+    {
+        switch (m_state)
+        {
+            case Idling:
+                write("Двери уже закрыты. Игнорируем запрос.");
+                break;
+            case Waiting:
+                m_doors.close();
+                break;
+            case MovingUp:
+            case MovingDown:
+                write("Кабина движется, двери уже закрыты. Игнорируем запрос.");
+                break;
+        }
+    }
+
+    void move_up()
+    {
+        switch (m_state)
+        {
+            case Idling:
+                write("Кабина начала движение вверх.");
+                update(MovingUp);
+                schedule_timer(m_moveTimer, CABIN_MOVE_ONE_FLOOR, [this]() {
+                    write("Кабина поднялась на этаж выше.");
+                    emit cabin_moved_up_signal();
+                });
+                break;
+            case Waiting:
+                write("Кабина ожидает. Пока нельзя начать движение вверх.");
+                break;
+            case MovingUp:
+                schedule_timer(m_moveTimer, CABIN_MOVE_ONE_FLOOR, [this]() {
+                    // FIXME: DRY
+                    write("Кабина поднялась на этаж выше.");
+                    emit cabin_moved_up_signal();
+                });
+                break;
+            case MovingDown:
+                write("Кабина не может изменить направление своего движения во время движения.");
+                break;
+        }
+    }
+
+    void move_down()
+    {
+        switch (m_state)
+        {
+            case Idling:
+                write("Кабина начала движение вниз.");
+                update(MovingDown);
+                schedule_timer(m_moveTimer, CABIN_MOVE_ONE_FLOOR, [this]() {
+                    write("Кабина опустилась на этаж ниже.");
+                    emit cabin_moved_down_signal();
+                });
+                break;
+            case Waiting:
+                write("Кабина ожидает. Пока нельзя начать движение вниз.");
+                break;
+            case MovingUp:
+                write("Кабина не может изменить направление своего движения во время движения.");
+                break;
+            case MovingDown:
+                schedule_timer(m_moveTimer, CABIN_MOVE_ONE_FLOOR, [this]() {
+                    // FIXME: DRY
+                    write("Кабина опустилась на этаж ниже.");
+                    emit cabin_moved_down_signal();
+                });
+                break;
+        }
+    }
+
+    void stop()
+    {
+        bool stopped = m_moveTimer.expiry() <= boost::asio::steady_timer::clock_type::now();
+
+        switch (m_state)
+        {
+            case Idling:
+                write("Кабина уже неподвижна.");
+                break;
+            case Waiting:
+                write("Кабина уже неподвижна. Ожидает.");
+                break;
+            case MovingUp:
+            case MovingDown:
+                if (stopped)
+                {
+                    write("Кабина остановилась.");
+                    update(Idling);
+                    emit cabin_stopped_signal();
+                }
+                else
+                {
+                    write("Кабина в движении, нельзя выполнить остановку.");
+                }
+                break;
+        }
+    }
+
+    State get_state() { return m_state; }
+
+    Doors::State get_doors_state() { return m_doors.get_state(); }
+
+    void set_on_cabin_moved_up_callback(Callback callback)
+    {
+        cabin_moved_up_signal = callback;
+    }
+
+    void set_on_cabin_moved_down_callback(Callback callback)
+    {
+        cabin_moved_down_signal = callback;
+    }
+
+    void set_on_cabin_stopped_callback(Callback callback)
+    {
+
+        cabin_stopped_signal = callback;
+    }
+
+private:
+    void update(State state)
+    {
+        m_state = state;
+    }
+
+    void write(std::string message)
+    {
+        message = "Кабина: " + message;
+        m_writer.write(message);
+    }
+
+    State m_state = Idling;
+    Doors m_doors;
+    Writer &m_writer;
+    boost::asio::io_context &m_context;
+    boost::asio::steady_timer m_moveTimer;
+
+    Callback cabin_moved_up_signal = [](){};
+    Callback cabin_moved_down_signal = [](){};
+    Callback cabin_stopped_signal = [](){};
+};
