bf0fa35cdc4cb8287ee0bfc7746d411506d679fc
Konstantin Runov <runebone1@gmail.com>
2023-06-08 20:44:05 +0300

    controller v2 works?

---
+++ lab4/controller.cpp
@@ -0,0 +1,338 @@
+#include "controller.h"
+
+#define NUMBER_OF_FLOORS 16
+
+Controller::Controller(boost::asio::io_context &ioContext, Writer &writer)
+    : m_context(ioContext),
+    m_writer(writer)
+{
+    m_cabin = std::make_shared<Cabin>(ioContext, writer);
+
+    s_should_update.connect([this](){ make_updating(); });
+    s_target_updated.connect([this](){ make_serving(); });
+    s_no_target.connect([this](){ make_idling(); });
+
+    s_cabin_should_move_up.connect([this](){
+            m_last_direction = Up;
+            m_cabin->make_moving_up();
+            });
+
+    s_cabin_should_move_down.connect([this](){
+            m_last_direction = Down;
+            m_cabin->make_moving_down();
+            });
+
+    s_cabin_should_be_waiting.connect([this](){ m_cabin->make_waiting(); });
+    s_cabin_should_be_idling.connect([this](){ m_cabin->make_idling(); });
+
+    m_cabin->s_moved_up.connect([this](){
+            increment_current_floor();
+            write("Кабина поднялась на этаж " + std::to_string(m_cur_floor) + ".");
+            make_serving();
+            });
+
+    m_cabin->s_moved_down.connect([this](){
+            decrement_current_floor();
+            write("Кабина опустилась на этаж " + std::to_string(m_cur_floor) + ".");
+            make_serving();
+            });
+
+    m_cabin->s_idling.connect([this](){ make_updating(); });
+}
+
+void Controller::activate_floor_button(int button, bool in_cabin)
+{
+    std::string s;
+
+    if (button < 1 || button > NUMBER_OF_FLOORS)
+    {
+        s = "Неверный этаж. (Доступно 1 - " + std::to_string(NUMBER_OF_FLOORS) + ")";
+        write(s);
+        return;
+    }
+
+    if (in_cabin)
+        m_cabin_btns |= (int)(std::pow(2, button - 1));
+    else
+        m_floor_btns |= (int)(std::pow(2, button - 1));
+
+    s = "Кнопка " + std::to_string(button) + " этажа активирована.";
+
+    if (in_cabin)
+        s += " (в кабине)";
+    else
+        s += " (на этаже)";
+
+    write(s);
+
+    s_should_update.emit();
+}
+
+void Controller::activate_cabin_cancel_button()
+{
+    if (!m_cabin_btns)
+    {
+        write("Никакие кнопки не нажаты.");
+    }
+    else
+    {
+        m_cabin_btns = 0;
+        write("Кнопки кабины сброшены.");
+        s_should_update.emit();
+    }
+}
+
+void Controller::deactivate_floor_button(int button, bool in_cabin)
+{
+    std::string s;
+
+    if (button < 1 || button > NUMBER_OF_FLOORS)
+    {
+        s = "Неверный этаж. (Доступно 1 - " + std::to_string(NUMBER_OF_FLOORS) + ")";
+        write(s);
+        return;
+    }
+
+    int bin_button = (int)(std::pow(2, button - 1));
+
+    // If button is active, deactivate it
+    if (in_cabin)
+    {
+        if (m_cabin_btns & bin_button)
+        {
+            m_cabin_btns ^= bin_button;
+            s = "Кнопка " + std::to_string(button) + " этажа деактивирована (в кабине).";
+            write(s);
+        }
+    }
+    else
+    {
+        if (m_floor_btns & bin_button)
+        {
+            m_floor_btns ^= bin_button;
+            s = "Кнопка " + std::to_string(button) + " этажа деактивирована (на этаже).";
+            write(s);
+        }
+    }
+}
+
+static int calculate_next_target_floor(int current_floor, int floor_btns_bin, int cabin_btns_bin, bool is_moving, bool up)
+{
+    int next_floor = 0;
+    int next_floor_up = 0;
+    int next_floor_down = 0;
+
+    for (int floor = current_floor + 1; 1 <= floor && floor <= NUMBER_OF_FLOORS; floor += 1) {
+        if (floor_btns_bin & (1 << (floor - 1)) || cabin_btns_bin & (1 << (floor - 1))) {
+            next_floor_up = floor;
+            break;
+        }
+    }
+
+    for (int floor = current_floor - 1; 1 <= floor && floor <= NUMBER_OF_FLOORS; floor -= 1) {
+        if (floor_btns_bin & (1 << (floor - 1)) || cabin_btns_bin & (1 << (floor - 1))) {
+            next_floor_down = floor;
+            break;
+        }
+    }
+
+    // Take next floor on the way up/down if the elevator is moving
+    if (is_moving)
+    {
+        if (up)
+        {
+            if (next_floor_up)
+            {
+                next_floor = next_floor_up;
+            }
+            // Cabin is moving up, but there are no floors above activated
+            else
+            {
+                next_floor = next_floor_down;
+            }
+        }
+        else
+        {
+            if (next_floor_down)
+            {
+                next_floor = next_floor_down;
+            }
+            else
+            {
+                next_floor = next_floor_up;
+            }
+        }
+        /* next_floor = up ? next_floor_up : next_floor_down; */
+    }
+    // If elevator is idling/waiting, take the nearest floor
+    else
+    {
+        if (next_floor_up && next_floor_down)
+        {
+            int cond = (next_floor_up - current_floor) < (current_floor - next_floor_down);
+
+            next_floor = cond ? next_floor_up : next_floor_down;
+        }
+        else if (next_floor_up)
+        {
+            next_floor = next_floor_up;
+        }
+        else if (next_floor_down)
+        {
+            next_floor = next_floor_down;
+        }
+        else
+        {
+            if (floor_btns_bin & (1 << (current_floor - 1)) || cabin_btns_bin & (1 << (current_floor - 1)))
+            {
+                next_floor = current_floor;
+            }
+            else
+            {
+                next_floor = 0;
+            }
+        }
+    }
+
+    return next_floor;
+}
+
+void Controller::update_target()
+{
+    bool is_moving = false;
+    bool up = false;
+
+    if (m_last_direction != None)
+    {
+        is_moving = true;
+
+        if (m_last_direction == Up)
+        {
+            up = true;
+        }
+        else
+        {
+            up = false;
+        }
+    }
+
+    int next_target = calculate_next_target_floor(m_cur_floor, m_floor_btns, m_cabin_btns, is_moving, up);
+
+    if (m_cur_target != next_target)
+    {
+        std::string s = "Текущая цель обновлена: " + std::to_string(m_cur_target) + " -> " + std::to_string(next_target);
+        m_cur_target = next_target;
+        write(s);
+    }
+}
+
+void Controller::make_idling()
+{
+    update_state(Idling);
+
+    m_last_direction = None;
+}
+
+void Controller::make_updating()
+{
+    update_state(Updating);
+
+    update_target();
+
+    if (target_exist())
+        s_target_updated.emit();
+    else
+        s_no_target.emit();
+}
+
+void Controller::make_serving()
+{
+    update_state(Serving);
+
+    if (target_exist())
+    {
+        if (m_cur_target > m_cur_floor)
+            s_cabin_should_move_up.emit();
+        else if (m_cur_target < m_cur_floor)
+            s_cabin_should_move_down.emit();
+        else
+        {
+            deactivate_floor_button(m_cur_floor, ON_FLOOR);
+            deactivate_floor_button(m_cur_floor, IN_CABIN);
+
+            update_target();
+
+            s_cabin_should_be_waiting.emit();
+        }
+    }
+    else
+    {
+        s_cabin_should_be_idling.emit();
+    }
+}
+
+void Controller::update_state(State new_state)
+{
+    m_state = new_state;
+}
+
+void Controller::write(std::string message)
+{
+    message = "Контроллер: " + message;
+    m_writer.write(message);
+}
+
+void Controller::print_state()
+{
+    std::string s;
+
+    switch (m_state)
+    {
+        case Idling:
+            s = "Простаивает.";
+            break;
+        case Updating:
+            s = "Обновляется.";
+            break;
+        case Serving:
+            s = "Обслуживает.";
+            break;
+    }
+
+    write("Состояние: " + s);
+
+    std::bitset<NUMBER_OF_FLOORS> floor_btns(m_floor_btns);
+    std::bitset<NUMBER_OF_FLOORS> cabin_btns(m_cabin_btns);
+
+    s = "Кнопки на этажах: " + floor_btns.to_string();
+    write(s);
+
+    s = "Кнопки в кабине : " + cabin_btns.to_string();
+    write(s);
+
+    s = "Текущий этаж: " + std::to_string(m_cur_floor);
+    write(s);
+
+    std::string target;
+
+    if (target_exist())
+        target = std::to_string(m_cur_target);
+    else
+        target = "Отсутствует.";
+
+    s = "Целевой этаж: " + target;
+    write(s);
+
+    s = "Предыдущее направление движения: ";
+
+    if (m_last_direction == None)
+        s += "Неподвижна.";
+    else if (m_last_direction == Up)
+        s += "Вверх.";
+    else if (m_last_direction == Down)
+        s += "Вниз.";
+
+    write(s);
+
+    m_cabin->print_state();
+}
---
+++ lab4/controller.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <string>
+#include <bitset>
+#include <boost/asio.hpp>
+
+#include "cabin.h"
+
+#include "writer.h"
+#include "timer.h"
+
+#include "signals_and_slots.h"
+
+#define IN_CABIN true
+#define ON_FLOOR false
+
+class Controller
+{
+    enum PrevMoveDirection { Down = -1, None = 0, Up = 1 };
+public:
+    enum State { Idling, Updating, Serving };
+
+    Controller(boost::asio::io_context &ioContext) : Controller(ioContext, Writer()) {}
+    Controller(boost::asio::io_context &ioContext, Writer &&writer) : Controller(ioContext, writer) {}
+    Controller(boost::asio::io_context &ioContext, Writer &writer);
+
+    void print_state();
+
+    void activate_floor_button(int button, bool in_cabin);
+    void activate_cabin_cancel_button();
+
+private:
+    void deactivate_floor_button(int button, bool in_cabin);
+
+private SLOTS:
+    void make_idling();
+    void make_updating();
+    void make_serving();
+
+private SIGNALS:
+    Signal<> s_should_update;
+    Signal<> s_target_updated;
+    Signal<> s_no_target;
+    Signal<> s_cabin_should_move_up;
+    Signal<> s_cabin_should_move_down;
+    Signal<> s_cabin_should_be_waiting;
+    Signal<> s_cabin_should_be_idling;
+
+private:
+    void increment_current_floor() { ++m_cur_floor; }
+    void decrement_current_floor() { --m_cur_floor; }
+
+    bool target_exist() { return m_cur_target != 0; }
+    void update_target();
+
+    void update_state(State new_state);
+    void write(std::string message);
+
+    State m_state = Idling;
+
+    Writer &m_writer;
+    boost::asio::io_context &m_context;
+
+    int m_floor_btns = 0b0000'0000'0000'0000;
+    int m_cabin_btns = 0b0000'0000'0000'0000;
+    int m_cur_floor  = 1;
+    int m_cur_target = 0;
+
+    PrevMoveDirection m_last_direction = None;
+
+public:
+    std::shared_ptr<Cabin> m_cabin;
+};
